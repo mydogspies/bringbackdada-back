b46c3136f9068314ef6b1d443342133cf5bbf1af
mydogspies <mydogspies@zoho.com>
2021-04-24 20:37:31 +0200

    Fixing connection error to remote db

--- model/index.js
+++ model/index.js
@@ -5,7 +5,6 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     port: dbConfig.PORT,
     dialect: dbConfig.dialect,
-    operatorsAliases: false,
 
     pool: {
         max: dbConfig.pool.max,
