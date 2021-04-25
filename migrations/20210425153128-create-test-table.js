66c245c68c2d2814aeb8da7a7ffa8e86afe0bb67
mydogspies <mydogspies@zoho.com>
2021-04-25 17:32:13 +0200

    Fixing connection error to remote db

---
+++ migrations/20210425153128-create-test-table.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('TestTables', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      test: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('TestTables');
+  }
+};
