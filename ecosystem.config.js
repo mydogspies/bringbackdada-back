3ec52f5d410f92cc7135d200e7a0078cf191a365
mydogspies <mydogspies@zoho.com>
2021-04-29 13:52:20 +0200

    Added changes to the code here and local hook to automatically pull and then reload PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps : [{
-        name: "bringbackdada-api",
+        name: "bringbackdada-api-server",
         script: "./server.js",
         env: {
             NODE_ENV: "production",
