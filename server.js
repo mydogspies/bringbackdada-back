5832cc63c5dd0f1644354b2395fb5c46621ca9fd
mydogspies <mydogspies@zoho.com>
2021-04-29 16:49:43 +0200

    Added changes to the code here and local hook to automatically pull and then reload PM2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const colors = require('colors');
 const errorHandler = require('./middleware/error');
 const db = require('./models');
 
-// testing full deployment and now and never 123456789
+// testing full deployment and now and never 1234567890
 
 // load env vars
 dotenv.config({path: './config/config.env'});
