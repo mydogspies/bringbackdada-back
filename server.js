02af51508cda8e520abad0a02ed8243b991bfd69
mydogspies <mydogspies@zoho.com>
2021-04-30 14:02:37 +0200

    Added changes to webhook logic

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const colors = require('colors');
 const errorHandler = require('./middleware/error');
 const db = require('./models');
 
-// test stuff 1
+// test stuff 12
 
 // load env vars
 dotenv.config({path: './config/config.env'});
